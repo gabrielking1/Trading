e740d08d4dbab8507539a9c14cfc5a66d8c45941
gabrielking1 <gkgabrielking@gmail.com>
2023-06-01 01:14:22 +0100

    4wee

--- Trader/settings.py
+++ Trader/settings.py
@@ -154,11 +154,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-# Configures the staticfiles directory to serve
-# static files from /static/ on our deployment
-STATIC_ROOT = os.path.join(
-    BASE_DIR, 'staticfiles', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
